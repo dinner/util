d7c9a9cc5034757f6bc968e3b0b424113c0be034
SingleCycle <zhanglingxiang123@gmail.com>
2017-04-17 14:26:05 +0800

    Update util.h

--- util.h
+++ util.h
@@ -2,8 +2,8 @@
 //  util.h
 //  videoPro
 //
-//  Created by zhanglingxiang on 15/10/9.
-//  Copyright (c) 2015年 zhanglingxiang. All rights reserved.
+//  Created by James on 15/10/9.
+//  Copyright (c) 2015年 James. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
